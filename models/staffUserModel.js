69bc9361410369bc4a48cdbcb3dda570ce1f9eff
qaidjoharj53 <qaidjoharytqt@gmail.com>
2024-09-09 01:45:45 +0530

    Refactor staff and user models' collection names

--- models/staffUserModel.js
+++ models/staffUserModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const staffUserSchema = mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  role: { type: String, default: "user" },
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+	role: { type: String, default: "user" },
 });
 
-const staffUserCollection = mongoose.model("users", staffUserSchema);
+const staffUserCollection = mongoose.model("staffusers", staffUserSchema);
 
 module.exports = { staffUserCollection };
--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true }, // Store the user's name
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    otp: { type: String },
-    otpExpiry: { type: Date },
-    isVerified: { type: Boolean, default: false },
-  },
-  { timestamps: true }
+	{
+		name: { type: String, required: true }, // Store the user's name
+		email: { type: String, required: true, unique: true },
+		password: { type: String, required: true },
+		otp: { type: String },
+		otpExpiry: { type: Date },
+		isVerified: { type: Boolean, default: false },
+	},
+	{ timestamps: true }
 );
 
-const userCollection = mongoose.model("user", userSchema);
+const userCollection = mongoose.model("users", userSchema);
 
 module.exports = { userCollection };
